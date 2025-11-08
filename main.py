26cec63df60ef71746091950927e889b485c07be
Alex <alex@hugyc.io>
2025-11-08 11:10:20 -0300

    Add .env-example and initialize main module
    Added a .env-example file to document required environment variables. Updated main.py to import necessary libraries and load environment variables, preparing for further development. Added langchain-community and tqdm to dependencies in pyproject.toml.

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from text-to-sql-chatbot-with-rag!")
+'''Main module for the Text-to-SQL Chatbot with RAG application.
+'''
+import os
+import sqlite3
+import hashlib
+from tqdm import tqdm
+from dotenv import load_dotenv
+from langchain_community.embeddings.huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
 
 
-if __name__ == "__main__":
-    main()
+# Load environment variables from .env file
+load_dotenv()
+
+
+# if __name__ == "__main__":
+#
+
